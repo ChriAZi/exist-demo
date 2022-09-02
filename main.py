12d8e02e9f5ea44fd80041b2cf4fc0bac6067970
Christopher Voit <development@christophervoit.de>
2022-09-02 12:14:26 +0200

    Adapted title

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Trail.ai Demo')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
 
 @st.cache
 def load_data(nrows):
